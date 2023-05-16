73aa54e407bbdcf55b4886102a27ed5f125b2d41
timkochura <90721761+positivebullshit@users.noreply.github.com>
2023-05-16 05:25:30 +0300

    Fixed problems with comments and allign symbols

--- Prak3.cpp
+++ Prak3.cpp
@@ -6,17 +6,23 @@
 
 using namespace std;
 
+const char * TW [] = { "program", "int", "string", "boolean", "false", "true", 
+    "and", "or", "not", "if", "else", "while", "break", "read", "write", NULL };
+
+const char * TD [] = { "{", "}", ";", ",", ":", "=", "(", ")", "==", "<", ">", 
+    "<=", "!=", ">=", "+",  "-", "*", "/", NULL };
+
 enum type_of_lex {
-    LEX_PROGRAM, 
-    LEX_INT, LEX_STRING, LEX_BOOLEAN, 
-    LEX_FALSE, LEX_TRUE,                                                                           
-    LEX_AND, LEX_OR, LEX_NOT, LEX_IF, LEX_ELSE, 
-    LEX_WHILE, LEX_BREAK, LEX_READ, LEX_WRITE, 
-    LEX_LCURLY, LEX_RCURLY, LEX_SEMICOLON, LEX_COMMA, LEX_COLON, LEX_ASSIGN, LEX_LPAREN, LEX_RPAREN, 
-    LEX_EQ, LEX_LESS, LEX_GREATER, LEX_LEQ, LEX_NEQ, LEX_GEQ, 
-    LEX_PLUS, LEX_MINUS, LEX_TIMES, LEX_SLASH,             
-    LEX_NUM, LEX_ID, LEX_STRLITERAL,
-    LEX_NULL,
+    LEX_PROGRAM,                                                                                            // 0
+    LEX_INT, LEX_STRING, LEX_BOOLEAN,                                                                       // 1 - 3
+    LEX_FALSE, LEX_TRUE,                                                                                    // 4 - 5
+    LEX_AND, LEX_OR, LEX_NOT, LEX_IF, LEX_ELSE,                                                             // 6 - 10
+    LEX_WHILE, LEX_BREAK, LEX_READ, LEX_WRITE,                                                              // 11 - 14
+    LEX_LCURLY, LEX_RCURLY, LEX_SEMICOLON, LEX_COMMA, LEX_COLON, LEX_ASSIGN, LEX_LPAREN, LEX_RPAREN,        // 15 - 22
+    LEX_EQ, LEX_LESS, LEX_GREATER, LEX_LEQ, LEX_NEQ, LEX_GEQ,                                               // 23 - 28
+    LEX_PLUS, LEX_MINUS, LEX_TIMES, LEX_SLASH,                                                              // 29 - 32          
+    LEX_NUM, LEX_ID, LEX_STRLITERAL,                                                                        // 33 - 35
+    LEX_NULL,                                                                                               // 36
 };
 
 class Lex {
@@ -30,40 +36,80 @@ public:
     int get_value() const {
         return v_lex;
     }
+    friend ostream& operator<< (ostream &s, Lex l); 
 };
-
-const char * TW [] = {"program", "int", "string", "boolean", "false", "true", 
-    "and", "or", "not", "if", "else", "while", "break", "read", "write", NULL};
-
-const char * TD [] = {"{", "}", ";", ",", ":", "=", "(", ")", "==", "<", ">", 
-    "<=", "!=", ">=", "+",  "-", "*", "/", NULL};
-
-int lex_num(string str, const char** id) {
-    int i = 0;
-    while ((id[i] != NULL) && (string(id[i]) != str)) {
-        i++;
-    }
-    if (id[i] == NULL) {
-        return -1;
-    }
-    return i;
-}
 
 vector <string> TID; 
 vector <string> TConst;
 
-Lex get_lex() {
+ostream& operator<< (ostream &s, Lex l) {
+    string t;
+    if (l.t_lex <= LEX_WRITE) {
+        t = TW[l.t_lex];
+    }
+    else if (l.t_lex <= LEX_SLASH) {
+        t = TD[l.t_lex - LEX_WRITE - 1];  
+    }
+    else if (l.t_lex == LEX_ID)
+    {
+        t = "IDENTIFICATOR with number";
+    }
+    else if (l.t_lex == LEX_STRLITERAL)
+    {
+        t = "STRLITERAL with number";
+        // t = TConst[l.v_lex];  //  This way if u want to print the string itself
+    }
+    else if (l.t_lex == LEX_NUM)
+    {
+        t = "NUMBER =";
+    }
+    else 
+        throw l;
+    s << t << " " << l.v_lex << endl;
+    return s;
+}
+
+class Scanner {
+    FILE* fp; 
+    char c, c_prev;
+    void gc() {
+        c = fgetc(fp);
+    }
+    char fpeek(FILE* fp) {
+        gc();
+        ungetc(c, fp);
+    }
+    int lex_num(string str, const char** id) {
+        int i = 0;
+        while ((id[i] != NULL) && (string(id[i]) != str)) {
+            i++;
+        }
+        if (id[i] == NULL) {
+            return -1;
+        }
+        return i;
+    }
+public:
+    Scanner(const char* program) {
+        if ( !(fp = fopen ( program, "r" )) ) 
+            throw "can't open file" ;
+    }
+    Lex get_lex ();
+};
+
+
+Lex Scanner::get_lex() {
     enum state {H, IDENT, NUMB, STRING, COM, COMP, SYMB};
-    char c;
-    int n;
-    string buf;
+    int n, i1, i2;
+    string buf, s1, s2;
     state CS = H;
-    do {
-        cin.get(c);
+    do { 
+        gc();
         switch (CS) {
+
             case H:
-                if (cin.eof()) {
-                    return Lex();
+                if (feof(fp)) {
+                    return Lex();  // 
                 }
                 if (c == ' ' || c == '\n' || c == '\t');
                 else if (isalpha(c)) {
@@ -74,43 +120,45 @@ Lex get_lex() {
                     n = c - '0';
                     CS = NUMB;
                 }
-                else if (c == '+'|| c == '-' || c == '*' || c == '{' || c == '}' 
-                     || c == ';' || c == ',' || c == ':' || c == '(' || c == ')') {
+                else if (c == '+' || c == '-' || c == '*' || c == '{' || c == '}' 
+                      || c == ';' || c == ',' || c == ':' || c == '(' || c == ')' ) {
                     CS = SYMB;
-                    cin.unget();
+                    ungetc(c, fp);
                 }
                 else if (c == '/') {
-                    if (cin.peek() == '*') {
-                        cin.get();
+                    c_prev = c;  // is necessary because we may ungetc twice
+                    if (fpeek(fp) == '*') {
+                        gc();
                         CS = COM;
                     }
                     else {
-                        cin.unget();
+                        ungetc(c_prev, fp);
                         CS = SYMB;
                     }
                 }
                 else if (c == '=' || c == '>' || c == '<' || c == '!') {
-                    if (cin.peek() == '=') {
+                    c_prev = c;  // is necessary because we may ungetc twice
+                    if (fpeek(fp) == '=') {
                         CS = COMP;
                     }
                     else {
                         CS = SYMB;
                     }
-                    cin.unget();
+                    ungetc(c_prev, fp);
                 }
                 else if (c == '"') {
                     CS = STRING;
                 }
                 else {
-                    throw -11111;
+                    throw "unexpected lex";
                 }
                 break;
+
             case IDENT:
                 if (isdigit(c) || isalpha(c)) {
                     buf.push_back(c);
                 }
                 else {
-                    cin.unget();
                     int i = lex_num(buf, TW);
                     if (i == -1) {
                         TID.push_back(buf);
@@ -121,32 +169,32 @@ Lex get_lex() {
                     }
                 }
                 break;
+
             case NUMB:
                 if (isdigit(c)) {
                     n = n * 10 + (c - '0');
                 }
                 else {
-                    cin.unget();
+                    ungetc(c, fp);
                     return Lex(LEX_NUM, n);
                 }
                 break;
+
             case COM:
-                if (cin.eof()) {
-                    throw -2;
+                if (feof(fp)) {
+                    throw "EOF inside comment";
                 }
                 if (c == '*') {
-                    if (cin.peek() == '/') {
-                        cin.get(c);
+                    if (fpeek(fp) == '/') {
+                        gc();
                         CS = H;
-                    }
-                    else {
-                        cin.unget();
                     }
                 }
                 break;
+
             case STRING:
-                if (cin.eof()) {
-                    throw -3;
+                if (feof(fp)) {
+                    throw "EOF inside string";
                 }
                 if (c != '"') {
                     buf.push_back(c);
@@ -155,34 +203,44 @@ Lex get_lex() {
                     return Lex(LEX_STRLITERAL, TConst.size() - 1);
                 }
                 break;
-            case COMP: {  // { } are necessary
-                string s1 = "";
+
+            case COMP:   // { } are necessary
                 s1.push_back(c);
-                s1.push_back(cin.get());
-                int i1 = lex_num(s1, TD);
-                return Lex((type_of_lex) (i1 + 15), i1);
+                gc();
+                s1.push_back(c);
+                i1 = lex_num(s1, TD);
+                return Lex((type_of_lex) (i1 + 15), i1 + 15);
                 break;
-                } 
+            
+
             case SYMB:
-                string ss = "";
-                ss.push_back(c);
-                int ii = lex_num(ss, TD);
-                return Lex((type_of_lex)(ii + 15), ii);
+                s2.push_back(c);
+                i2 = lex_num(s2, TD);
+                return Lex((type_of_lex)(i2 + 15), i2 + 15);
                 break;
         }
     } while (true);
 }
 
 int main(int argc, char** argv) {
-    Lex lex;
-    while (true) {
-        lex = get_lex();
-        if (lex.get_type() == LEX_NULL) {
-            break;
+    try {
+        Scanner p("program.txt");
+        Lex temp;
+        while (true) {
+            temp = p.get_lex();
+            if (temp.get_type() == LEX_NULL) {
+                break;
+            }
+            cout << temp;
         }
-        else {
-            cout << lex.get_type() << " " << lex.get_value()<< endl;
-        }
+        return 0;
     }
-    return 0;
+
+    catch (const char* str) {
+        cout << "Code terminated: " << str << endl;
+    }
+
+    catch (Lex l) {
+        cout << "Unexpected Lexeme " << l << endl;
+    }
 }
